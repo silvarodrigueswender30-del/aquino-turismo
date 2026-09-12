9bc5eded3192b45ea6d58c8b6951c58af39aa1f9
Wender Silva Rodrigues <silvarodrigueswender30@gmail.com>
2026-09-12 09:57:29 -0300

    style(tickets): increase breathing room padding between vertical dashed divider and lateral barcode

--- src/components/ui/feature-grid-enterprise-grade.tsx
+++ src/components/ui/feature-grid-enterprise-grade.tsx
@@ -98,7 +98,7 @@ const FeatureGrid: React.FC<FeatureGridProps> = ({ className }) => {
             </div>
 
             {/* aba do barcode lateral */}
-            <div className="flex w-12 flex-shrink-0 items-center justify-center py-6 pr-2 md:w-14 lg:w-16">
+            <div className="flex w-12 flex-shrink-0 items-center justify-center py-6 pl-4 pr-2 md:w-14 md:pl-5 lg:w-16 lg:pl-6">
               <Barcode className="h-[85%] w-full text-ocean-navy" />
             </div>
           </div>
